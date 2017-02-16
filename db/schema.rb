d8aab96d1dd5e3a79f4326ac42e8189ae0fd5795
Diogo Farias Rodrigues <diogofgr@gmail.com>
2017-02-16 11:51:52 +0000

    Done scaffold cocktails

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216113421) do
+ActiveRecord::Schema.define(version: 20170216114733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
+  create_table "cocktails", force: :cascade do |t|
+    t.string   "name"
+    t.string   "ingredient"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
