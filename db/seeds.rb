9af85566ef5f13e3f81ac5e8a65a42823bb26a9b
Diogo Farias Rodrigues <diogofgr@gmail.com>
2017-02-17 16:05:46 +0000

    Add some styling to the index

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,31 @@ all_ingredients.each do |ingredient|
   Ingredient.create(name: ingredient.capitalize)
 end
 
-# # Seed with one expample cocktail
-#   url = "http://placehold.it/400x400"
-# (0..5).each do |i|
-#   product = Cocktail.new(name: "Cocktail #{i}")
-#   product.save!
-#   product.photo_url = url # Upload happens here
-# end
+# Seed with example cocktails
+urls = [
+  "mojito_juqqug.jpg",
+  "tequilla-sunrise_nfo157.jpg",
+  "san-francisco_pfuods.jpg",
+  "pinacolada_yxbdyu.jpg",
+  "gin-titanic_w5uzqm.jpg"
+]
+cocktail_1 = Cocktail.new(name: "Mojito")
+cocktail_1.save!
+cocktail_1.photo_url = "http://res.cloudinary.com/dpfv7sbu4/image/upload/v1487341784/#{urls[0]}"
+
+cocktail_2 = Cocktail.new(name: "Tequilla Sunrise")
+cocktail_2.save!
+cocktail_2.photo_url = "http://res.cloudinary.com/dpfv7sbu4/image/upload/v1487341784/#{urls[1]}"
+
+cocktail_3 = Cocktail.new(name: "San Francisco")
+cocktail_3.save!
+cocktail_3.photo_url = "http://res.cloudinary.com/dpfv7sbu4/image/upload/v1487341784/#{urls[2]}"
+
+cocktail_4 = Cocktail.new(name: "Pina Colada")
+cocktail_4.save!
+cocktail_4.photo_url = "http://res.cloudinary.com/dpfv7sbu4/image/upload/v1487341784/#{urls[3]}"
+
+cocktail_5 = Cocktail.new(name: "Titanic Gin")
+cocktail_5.save!
+cocktail_5.photo_url = "http://res.cloudinary.com/dpfv7sbu4/image/upload/v1487341784/#{urls[4]}"
+
